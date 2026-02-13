8b6d5d5a2a6515eadc41cce4727e4612681e420c
CadeMichael <cade.lueker@gmail.com>
2026-02-13 09:20:54 -0700

    parameterize Prog with 'P' in Elim handlers

---
+++ pylang/tests/testDeepIf.py
@@ -0,0 +1,12 @@
+
+count = 0
+i = 0
+if i == i:
+    if i < i + 1:
+        if i <= i:
+            if i + 1 > i:
+                if i >= i:
+                    count = count + 1
+    i = i + 1
+
+#count==1
